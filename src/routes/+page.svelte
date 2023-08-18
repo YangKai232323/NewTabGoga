be6818a4177194b467221314c2c321c5ee8e1e1a
YangKai232323 <rodontop1@gmail.com>
2023-08-18 09:30:27 +0300

    fix: prevent default on any btns

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,32 @@
 <script lang="ts">
-    let search = ''
-    let input: HTMLInputElement 
+	let search = '';
+	let input: HTMLInputElement;
 	function onKeyDown(e: any) {
 		switch (e.key) {
 			case 'Tab':
 				console.log('Tab');
+				e.preventDefault();
+				break;
+			case 'Alt':
+				e.preventDefault();
 				break;
 			case 'Enter':
 				console.log('Enter');
-                let url = `https://google.com/search?q=${encodeURIComponent(search.replaceAll(' ', '+'))}`
-                window.open(url)
+				let url = `https://google.com/search?q=${encodeURIComponent(search.replaceAll(' ', '+'))}`;
+				window.open(url);
 				break;
-            
-            default:
-                if ((e.keyCode >=48 && e.keyCode<=90) || (e.keyCode >= 96 && e.keyCode <= 111) || (e.keyCode>= 186 && e.keyCode <= 222)) {
-                    input.focus()
-                    break;
-                }
-                else {
-                    break
-                }
 
+			default:
+				if (
+					(e.keyCode >= 48 && e.keyCode <= 90) ||
+					(e.keyCode >= 96 && e.keyCode <= 111) ||
+					(e.keyCode >= 186 && e.keyCode <= 222)
+				) {
+					input.focus();
+				}
+				break;
 		}
 	}
-
 </script>
 
 <div class="main">
@@ -36,7 +39,7 @@
 		<p><kbd>Enter</kbd> for search your question</p>
 	</div>
 
-	<input type="text" placeholder="Search here" bind:value={search} bind:this={input}/>
+	<input type="text" placeholder="Search here" bind:value={search} bind:this={input} />
 </div>
 
 <svelte:window on:keydown={onKeyDown} />
@@ -55,4 +58,3 @@
 		font-size: large;
 	}
 </style>
-
